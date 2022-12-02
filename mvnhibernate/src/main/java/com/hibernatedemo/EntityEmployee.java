599f1403e4eb18592896c2b448d5f79ea29e3789
Soft <Soft@Akshay>
2022-12-02 18:24:16 +0530

    commit

---
+++ mvnhibernate/src/main/java/com/hibernatedemo/EntityEmployee.java
@@ -0,0 +1,49 @@
+package com.hibernatedemo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="Employee_inf")
+public class EntityEmployee {
+	
+	@Id
+	private int id;
+	@Column(name="emp_name")
+	private String name;
+	@Transient
+	private String city;
+	public EntityEmployee(int id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
